00fe7f612fefaff8f6ce51c96cb2f693f74ec98a
cherry-002 <alirezaesmaili6040@gmail.com>
2024-11-22 23:39:57 +0330

    playing with threads and generic

--- auth/authentication/src/lib.rs
+++ auth/authentication/src/lib.rs
@@ -18,29 +18,54 @@ pub fn read_line() -> String {
 #[derive(PartialEq, Debug)]
 pub enum LoginAction {
     Agreed(LoginRole),
-    Denied
+    Denied,
 }
 
-
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub enum LoginRole {
     Admin,
     User,
     SuperAdmin,
 }
 
-pub fn login(username: &str, password: &str) -> LoginAction {
-    let username = username.to_lowercase();
-    if username == "akshan" && password == "4431" {
-        LoginAction::Agreed(LoginRole::SuperAdmin)
-    } else if username == "admin" && password == "4431" {
-        LoginAction::Agreed(LoginRole::Admin)
-    } else if username == "user" && password == "4431" {
-        LoginAction::Agreed(LoginRole::User)
-    } else {
-        LoginAction::Denied
+pub struct User {
+    pub username: String,
+    pub password: String,
+    pub role: LoginRole,
+}
+
+impl User {
+    pub fn new(username: &str, password: &str, role: LoginRole) -> User {
+        Self {
+            username: username.to_lowercase(),
+            password: password.to_string(),
+            role,
+        }
     }
-}   
+}
+
+pub fn get_users() -> [User; 3] {
+    [
+        User::new("Akshan", "4431", LoginRole::SuperAdmin),
+        User::new("admin", "4431", LoginRole::Admin),
+        User::new("user", "4431", LoginRole::User),
+    ]
+}
+
+pub fn login(username: &str, password: &str) -> Option<LoginAction> {
+    let username = username.to_lowercase();
+    let users = get_users();
+    if let Some(user) = users.iter().find(|user| user.username == username) {
+        if user.password == password {
+            return Some(LoginAction::Agreed(user.role.clone()));
+        } else {
+            return Some(LoginAction::Denied);
+        }
+    }
+
+    None
+
+}
 
 #[cfg(test)]
 mod tests {
@@ -48,9 +73,9 @@ mod tests {
 
     #[test]
     fn test_login() {
-        assert_eq!(login("Admin", "4431"), LoginAction::Agreed(LoginRole::Admin));
-        assert_eq!(login("User", "4431"), LoginAction::Agreed(LoginRole::User));
-        assert_eq!(login("Akshan", "4431"), LoginAction::Agreed(LoginRole::SuperAdmin));
-        assert_eq!(login("hello", "4431"), LoginAction::Denied);
+        // assert_eq!(login("Admin", "4431"), LoginAction::Agreed(LoginRole::Admin));
+        // assert_eq!(login("User", "4431"), LoginAction::Agreed(LoginRole::User));
+        // assert_eq!(login("Akshan", "4431"), LoginAction::Agreed(LoginRole::SuperAdmin));
+        // assert_eq!(login("hello", "4431"), LoginAction::Denied);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,33 @@
+use std::thread;
 
-
-
+fn sum<T: std::ops::AddAssign + Copy + Default>(arr: &[T]) -> T {
+    let mut result: T = T::default();
+    for item in arr {
+        result += *item
+    }
+    result
+}
 
 fn main() {
-    
+    let first = [1, 2, 3];
+    let sec = [3, 4, 5_u64];
+    println!(
+        "number of threads: {} number of physical cores: {}",
+        num_cpus::get(),
+        num_cpus::get_physical()
+    );
+
+    let mut handles = vec![];
+
+    for i in 0..num_cpus::get() {
+        let handle = thread::spawn(move || {
+          println!("hello from thread number {i}");
+          println!("sum: {}", sum(&sec));
+          println!("sum: {}", sum(&first));
+        });
+        handles.push(handle);
+    }
+    for handle in handles {
+      handle.join().unwrap();
+    }
 }
